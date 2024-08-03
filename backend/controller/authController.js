5b166c88d3f792e0272a6185d146a162f15e75c4
Gopisha18072003 <Gopishaw111@gmail.com>
2024-08-03 15:45:32 +0530

    updated auth

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -33,7 +33,7 @@ const createSendToken = (user, statusCode, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
     ),
     httpOnly: true,
-    sameSite: 'Strict',
+    sameSite: 'None',
     secure: true, // Ensure secure cookies in production
   };
 
